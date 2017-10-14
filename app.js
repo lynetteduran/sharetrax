4fcb7b4294b592c8959860b14962bf7aa3cf91e6
Lynette Duran <lyndduran@gmail.com>
2017-10-13 18:34:09 -0700

    requires neo-4j-driver.v1

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
+var neo4j = require('neo4j-driver').v1;
 
 //View Engine
 app.set('views', path.join(__dirname, 'views'));
@@ -15,10 +16,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+var driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', 'neo4j'));
 app.get('/', function(req, res){
   res.send('It Works');
 });
+var session = driver.session();
 
 app.listen(port);
 console.log('I can hear you breathe...on port:' + port);
+
 module.exports = app;
